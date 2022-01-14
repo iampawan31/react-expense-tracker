65cad6a88f2acba8d3b75f5c9fa0747d5c01a31e
Pawan Kumar <surpawan@gmail.com>
2022-01-14 13:46:18 +0530

    Added Initial Layout

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
 import './App.css'
+import { Stack, Container, Button } from 'react-bootstrap'
+import BudgetCard from './components/BudgetCard'
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
+    <Container className="my-4">
+      <Stack direction="horizontal" gap={2} className="mb-4">
+        <h1 className="me-auto">Budgets</h1>
+        <Button variant="success" onClick={() => console.log('Primary')}>
+          Add Budget
+        </Button>
+        <Button
+          variant="outline-success"
+          onClick={() => console.log('Primary')}
+        >
+          Add Budget
+        </Button>
+      </Stack>
+      <div
+        style={{
+          display: 'grid',
+          gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))',
+          gap: '1rem',
+          alignItems: 'flex-start',
+        }}
+      >
+        <BudgetCard name="Entertainment" amount={1000} max={3000}></BudgetCard>
+      </div>
+    </Container>
   )
 }
 
